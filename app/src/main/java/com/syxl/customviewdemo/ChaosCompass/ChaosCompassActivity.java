8c07e719d4760d3197e97fd9a1fc81896e2d00ba
李昆 <472291341@qq.com>
2018-03-06 23:59:40 +0800

    小米指南针

---
+++ app/src/main/java/com/syxl/customviewdemo/ChaosCompass/ChaosCompassActivity.java
@@ -0,0 +1,15 @@
+package com.syxl.customviewdemo.ChaosCompass;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.syxl.customviewdemo.R;
+
+public class ChaosCompassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chaos_compass);
+    }
+}
